b27ed5fc0bfc3addc65d2f2dadd6f68ba2755e6f
cetiefp <86770936+cetiefp@users.noreply.github.com>
2026-05-17 17:26:18 +0100

    Update gerar_data.py
    rev11

--- gerar_data.py
+++ gerar_data.py
@@ -9,7 +9,7 @@ TOTAL = 20
 ESTADO_FILE = "estado.json"
 
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36",
+    "User-Agent": "Mozilla/5.0",
     "Accept-Language": "pt-PT,pt;q=0.9"
 }
 
@@ -22,24 +22,3 @@ def carregar_estado():
 
 
 def guardar_estado(cod):
-    with open(ESTADO_FILE, "w", encoding="utf-8") as f:
-        json.dump({"ultimo_cod": cod}, f)
-
-
-def get_oferta(cod):
-    url = BASE_URL + str(cod)
-
-    try:
-        r = requests.get(url, headers=HEADERS, timeout=3)
-
-        if r.status_code != 200:
-            return None
-
-        html = r.text
-
-        if "Pesquisar Oferta" in html or "Oferta_Pesquisa" in html:
-            return None
-
-        soup = BeautifulSoup(html, "html.parser")
-
-        titulo_tag = soup.find("span", id="ctl00_ContentPlaceHolder1_lblDesignacao")
